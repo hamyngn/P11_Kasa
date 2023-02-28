af9ddef97782128c75e0ff7f8d4726c9e6268858
Ha My Nguyen <hamyngn@gmail.com>
2023-02-28 13:31:47 +0100

    modify logement page

--- src/pages/LogementPage.js
+++ src/pages/LogementPage.js
@@ -1,20 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Logement from '../components/Logement';
 import data from '../data/data.js'
 import { useParams, Navigate } from 'react-router-dom';
 
 const LogementPage = () => {
     const id = useParams().id;
-    const [isError, setError] = useState(false)
-    
     const logement = data.find(i => {return i.id === id})
-    if(logement === undefined) {
-        setError (true)
-    }
+
     return (
         <>
-        {isError && <Navigate to="/error" replace={true} />}
-        <Logement logement={logement} />
+        {logement === undefined && <Navigate to="/error" replace={true} />}
+        {logement !== undefined && <Logement logement={logement} />}
         </>
     )  
 }
